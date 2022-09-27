d9d49fbf292c3778592f708af38073fe69cae086
John <jhcao23@gmail.com>
2022-09-27 11:15:07 -0400

    refac filename

--- js/vr.js
+++ js/vr.js
@@ -1,28 +1,29 @@
-            const createNewScene = function (canvasId, filename) {
-                const theCanvas = document.getElementById(canvasId); // Get the canvas element
-                const theEngine = new BABYLON.Engine(theCanvas, true); // Generate the BABYLON 3D engine
-                var theScene = new BABYLON.Scene(theEngine);
-                
-                var camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI / 2,  Math.PI / 2, 5, BABYLON.Vector3.Zero(), theScene);
+const createNewScene = function (canvasId) {
+    const theCanvas = document.getElementById(canvasId); // Get the canvas element
+    const filename0 = theCanvas.attributes['filename'];
+    const filename = `../${filename0.value}`;
+    const theEngine = new BABYLON.Engine(theCanvas, true); // Generate the BABYLON 3D engine
+    var theScene = new BABYLON.Scene(theEngine);
+    
+    var camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI / 2,  Math.PI / 2, 5, BABYLON.Vector3.Zero(), theScene);
 
-                camera.attachControl(theCanvas, true);
-                camera.inputs.attached.mousewheel.detachControl(theCanvas);
+    camera.attachControl(theCanvas, true);
+    camera.inputs.attached.mousewheel.detachControl(theCanvas);
 
-                var dome = new BABYLON.PhotoDome(
-                    "testdome",
-                    filename,
-                    {
-                        resolution: 32,
-                        size: 1000
-                    },
-                    theScene
-                );    
-                // return theScene;
-                theEngine.runRenderLoop(function () {
-                    theScene.render();
-                });
-            };
+    var dome = new BABYLON.PhotoDome(
+        "testdome",
+        filename,
+        {
+            resolution: 32,
+            size: 1000
+        },
+        theScene
+    );    
+    // return theScene;
+    theEngine.runRenderLoop(function () {
+        theScene.render();
+    });
+};
 
-            createNewScene('renderCanvas0', '../2nd_floor.jpg'); //Call the createScene function
-            createNewScene('renderCanvas1', '../361.jpg'); //Call the createScene function
-            
+createNewScene('renderCanvas0'); //Call the createScene function
+createNewScene('renderCanvas1'); //Call the createScene function
