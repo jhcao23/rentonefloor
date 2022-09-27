edb3e5295a31a414e527ad68a6bcb7b28c28055b
John <jhcao23@gmail.com>
2022-09-27 11:00:40 -0400

    refac js

--- js/vr.js
+++ js/vr.js
@@ -1,25 +1,12 @@
-            // const canvas = document.createElement("canvas");            
-            const canvas = document.getElementById("renderCanvas0"); // Get the canvas element
-            const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
-
-            const canvas1 = document.getElementById("renderCanvas1"); // Get the canvas element
-            const engine1 = new BABYLON.Engine(canvas1, true); // Generate the BABYLON 3D engine
-
-            // const canvas2 = document.getElementById("renderCanvas2"); // Get the canvas element
-            // const engine2 = new BABYLON.Engine(canvas2, true); // Generate the BABYLON 3D engine
-
-            // const canvas3 = document.getElementById("renderCanvas3"); // Get the canvas element
-            // const engine3 = new BABYLON.Engine(canvas3, true); // Generate the BABYLON 3D engine
-            
-            // engine.inputElement = document.getElementById("renderCanvas0");
-
-            const createScene = function (filename, theEngine) {
+            const createNewScene = function (canvasId, filename) {
+                const theCanvas = document.getElementById(canvasId); // Get the canvas element
+                const theEngine = new BABYLON.Engine(theCanvas, true); // Generate the BABYLON 3D engine
                 var theScene = new BABYLON.Scene(theEngine);
-
+                
                 var camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI / 2,  Math.PI / 2, 5, BABYLON.Vector3.Zero(), theScene);
 
-                camera.attachControl(canvas, true);
-                camera.inputs.attached.mousewheel.detachControl(canvas);
+                camera.attachControl(theCanvas, true);
+                camera.inputs.attached.mousewheel.detachControl(theCanvas);
 
                 var dome = new BABYLON.PhotoDome(
                     "testdome",
@@ -29,25 +16,13 @@
                         size: 1000
                     },
                     theScene
-                );
-
-                // theScene.createDefaultEnvironment();
-    
-                return theScene;
+                );    
+                // return theScene;
+                theEngine.runRenderLoop(function () {
+                    theScene.render();
+                });
             };
 
-            const scene = createScene('../2nd_floor.jpg', engine); //Call the createScene function
-            const scene1 = createScene('../361.jpg', engine1); //Call the createScene function
-            // const scene2 = createScene('./360photo.jpg', engine2); //Call the createScene function
-            // const scene3 = createScene('./360photo.jpg', engine3); //Call the createScene function
-
-            // Register a render loop to repeatedly render the scene
-            engine.runRenderLoop(function () {
-                scene.render();
-                scene1.render();
-                // scene2.render();
-                // scene3.render();
-                // if (scene.activeCamera) {
-                //     scene.render();
-                // }
-            });
+            createNewScene('renderCanvas0', '../2nd_floor.jpg'); //Call the createScene function
+            createNewScene('renderCanvas1', '../361.jpg'); //Call the createScene function
+            
